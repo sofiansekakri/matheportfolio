f939299b5c8c5029fc26622b0e4a7f398d2e6d7a
Sofian S. <143905862+sofiansekakri@users.noreply.github.com>
2025-02-27 11:25:32 +0100

    Aktualisieren von js

---
+++ js/testen.js
@@ -0,0 +1,30 @@
+const number = 
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const input = document.getElementById('numberInput').value;
+
+if (number[0] === input) {
+
+} if else (number[1] === input) {
+
+} if else (number[2] === input) {
+
+} if else (number[3] === input) {
+
+} if else (number[4] === input) {
+
+} if else (number[5] === input) {
+
+} if else (number[6] === input) {
+
+} if else (number[7] === input) {
+
+} if else (number[8] === input) {
+
+} if else (number[9] === input) {
+
+} else {
+const n = 'Keine Nummer!';
+console.log(n);
+noNumber();
+}
